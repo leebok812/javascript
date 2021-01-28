630922fa4b5be071b6e0a99e3c6947bb283a95ca
JUNEBOK <leebok0812@gmail.com>
2021-01-28 16:07:26 +0900

    간단한 계산기 프로그램 + 선택자 공부

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -121,7 +121,7 @@
                         });
 
 
-                        //EX6 : EX6 : 엘리먼트 노드의 속성 변경 예제 - 사진 변경 & CSS속성변경
+                        //EX6 :  엘리먼트 노드의 속성 변경 예제 - 사진 변경 & CSS속성변경
                     /* window.onload = function (){ onload의 중복으로 인해 변경 다른사람의 코드를 망가뜨리지 않으려면 addEventListener사용*/  
                         window.addEventListener("load", function(){
                             var section6=document.querySelector("#section6"); // .은 클래스 #은 id 
@@ -150,7 +150,7 @@
                         
                         });
 
-                         //EX7 : EX7 : 노드조작: 메뉴추가(createTextNode, Element)
+                         //EX7 :  노드조작: 메뉴추가(createTextNode, Element)
                     /* window.onload = function (){ onload의 중복으로 인해 변경 다른사람의 코드를 망가뜨리지 않으려면 addEventListener사용*/  
                         window.addEventListener("load", function(){
                             var section7=document.querySelector("#section7"); // .은 클래스 #은 id 
@@ -171,6 +171,76 @@
                                     var txtNode = menuListDiv.childNodes[0]; // 예를 위하여 이렇게 작성했지만 이렇게는 거의 쓰지 않는다.
                                     menuListDiv.removeChild(txtNode);
 
+
+                                };
+                              
+                        
+                        
+                        });
+                      
+                      
+                        //EX8 :   엘리먼트 노드 추가(appendChild,append,innerHTML)/삭제(removeChild,remove)그리고 주의할 점들
+                    /* window.onload = function (){ onload의 중복으로 인해 변경 다른사람의 코드를 망가뜨리지 않으려면 addEventListener사용*/  
+                        window.addEventListener("load", function(){
+                            var section8=document.querySelector("#section8"); // .은 클래스 #은 id 
+                        
+                            var titleInput2 = section8.querySelector(".title-input2");// .은 클래스 #은 id 
+                            var menuListUl = section8.querySelector(".menu-list2");// .은 클래스 #은 id 
+                            var addButton2 = section8.querySelector(".add-button2");// .은 클래스 #은 id 
+                            var delButton2= section8.querySelector(".del-button2");// .은 클래스 #은 id 
+
+                                addButton2.onclick = function(){ 
+
+                                    var title2 = titleInput2.value;
+                                    //1번 방법 2번 방법의 문제를 해결하기위한 선택
+                                    var html = '<a href="">'+title2+'</a>';
+                                    var li = document.createElement("li");
+                                    li.innerHTML = html;
+
+                                    menuListUl.appendChild(li);
+
+                                    /* 2번 방법
+                                    menuListUl.innerHTML += '<li><a href="">'+title2+'</a></li>'; // 따옴표 주의
+                                    //한번쓰고 말고는 상관없으나 (문제없음)
+                                    //내부적 문제 발생할수도 : 문자열 + 문자열 + 문자열 ...... 건물을 헐고 짓고 헐고 짓고 나중에는 객체가 많아서 성능상 문제 발생할수도.
+                                        */
+
+                                    /* 3번 방법 직접 생성되는 형식으로 만드는 방식
+                                      코드가 기나 위와 달리 성능상에 문제에서는 바람직 하다.
+                                    var title2 = titleInput2.value;
+                                    var txtNode2 = document.createTextNode(title2);
+                                    
+                                    var aNode = document.createElement("a");
+                                    aNode.href="";
+                                    aNode.appendChild(txtNode2);
+
+                                    var liNode = document.createElement("li");
+                                    liNode.appendChild(aNode);
+                                    
+                                    menuListUl.appendChild(liNode);
+                                    */
+
+                                        /* EX7 방식
+                                        var title2 = titleInput2.value;
+                                        var txtNode2 = document.createTextNode(title2);
+                                        menuListDiv2.appendChild(txtNode2);
+                                        */
+                                };
+                               
+                                delButton2.onclick = function(){ 
+                                   
+                                   /*EX7에서 썼던 방식
+                                    var txtNode2 = menuListDiv2.childNodes[0]; // 모든 노트들에서 0번째 | 예를 위하여 이렇게 작성했지만 이렇게는 거의 쓰지 않는다.
+                                    menuListUl.removeChild(txtNode2);
+                                        */
+                                
+                                        /* 1.부모를 얻어서 그 기능을 통해 삭제 하는 방법 
+                                    var liNode = menuListUl.children[0];//앨리먼트 노트들만 선별해서 그중에서 첫번째
+                                    menuListUl.removeChild(liNode);
+                                        */
+                                    //2. 자기자신이 삭제 되는 방법.
+                                       var liNode = menuListUl.children[0];
+                                       liNode.remove();
 
                                 };
                               
